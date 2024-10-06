72854dc4b8302f74065cf0a88a3f15428d9ab3d2
Mikhail Lisovskii <f1ste@yandex.ru>
2024-10-07 00:56:37 +0300

    Вынос публичных роутов

--- routes/web.php
+++ routes/web.php
@@ -18,10 +18,12 @@ use App\Http\Controllers\Admin\SongController;
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('template');;
 Route::get('/search', [App\Http\Controllers\SearchController::class, 'index'])->name('search');
 
-    Route::get('/home', function(){
-        return Redirect::to('/admin_panel/catalogs');
-    });
+Route::get('/home', function(){
+    return Redirect::to('/admin_panel/catalogs');
+});
 
+Route::get('/songs/fetch', [CatalogController::class, 'fetchSongs'])->name('songs.fetch');
+Route::get('/song-search', [CatalogController::class, 'fetchSongs'])->name('songs.search');
 
 Route::middleware(['auth'])->prefix('admin_panel')->group(function () {
     Route::get('/payment/fail', [App\Http\Controllers\Admin\PaymentController::class, 'fail'])->name('payment.fail');
@@ -42,10 +44,8 @@ Route::middleware(['auth'])->prefix('admin_panel')->group(function () {
     Route::get('/profile', [App\Http\Controllers\Admin\ProfileController::class, 'index'])->name('profile');
     Route::post('/profile', [App\Http\Controllers\Admin\ProfileController::class, 'update'])->name('profile.update');
     Route::get('/help-center', [App\Http\Controllers\Admin\HelpCenterController::class, 'index'])->name('help');
-    Route::get('/songs/fetch', [CatalogController::class, 'fetchSongs'])->name('songs.fetch');
     Route::post('/catalog/songs', [SongController::class, 'store'])->name('songs.store');
     Route::delete('/catalog/songs/{song}', [SongController::class, 'destroy'])->name('songs.destroy');
-    Route::get('/song-search', [CatalogController::class, 'fetchSongs'])->name('songs.search');
 });
 
  Route::get('/clear-cache', function() {
