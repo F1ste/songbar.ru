0a40efa139b5a1fd4efc90bbde7a90c7b4603701
Mikhail Lisovskii <f1ste@yandex.ru>
2024-10-26 03:27:20 +0300

    лэйаут для заглушек и заглушки

--- resources/views/success_payment.blade.php
+++ resources/views/success_payment.blade.php
@@ -1,17 +1,19 @@
 @extends('layouts.dummy_layout')
 
 @section('content')
-<div class="container mt-5">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card text-center">
-                <div class="card-body">
-                    <h1 class="display-4 text-success">Платеж прошел успешно!</h1>
-                    <p class="lead">Спасибо за ваш платеж. Ваш заказ был успешно обработан.</p>
-                    <a href="{{ route('catalogs') }}" class="btn btn-primary mt-3">К каталогам</a>
+<main class="py-4 d-flex align-items-center" style="min-height: 100%;">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card text-center">
+                    <div class="card-body">
+                        <h1 class="display-5 text-success">Платеж прошел успешно!</h1>
+                        <p class="lead">Спасибо за ваш платеж. Ваш заказ был успешно обработан.</p>
+                        <a href="https://songbar.ru/home/" class="btn btn-primary mt-3">На главную</a>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
+</main>
 @endsection
